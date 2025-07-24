53f0a9ca859d396c07ab3ba64ce50ecb19366009
Heng1222 <maxbeettww@gmail.com>
2025-07-24 15:00:57 +0800

    修正py檔案

--- kling_authentication/main.py
+++ kling_authentication/main.py
@@ -21,11 +21,14 @@ def authenticate(data: AuthRequest):
         "alg": "HS256",
         "typ": "JWT"
     }
+    if not sk or not ak:
+        raise Exception("未正確載入sk or ak，請檢查 .env 設定")
+
     payload = {
         "iss": ak,
         "exp": int(time.time()) + data.number, # The valid time, in this example, represents the current time+1800s(30min)
         "nbf": int(time.time()) # The time when it starts to take effect, in this example, represents the current time minus 5s
     }
     token = jwt.encode(payload, sk, headers=headers)
-    return token
+    return str(token)
 #----------------
